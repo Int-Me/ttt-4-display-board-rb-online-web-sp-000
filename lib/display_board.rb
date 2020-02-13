5c73100198ef2615822d1c1043e5f4da3d4ec1c2
Roland McRae <mcraeroland@gmail.com>
2020-02-13 02:59:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  firstline = board[0] + "|" + board[1] + "|" + board[2]
-  secondline = board[3] + "|" + board[4] + "|" + board[5]
-  thirdline = board[6] + "|" + board[7] + "|" + board[8]
+  firstline = " " + board[0] + " | " + board[1] + " | " + board[2] + " "
+  secondline = " " + board[3] + " | " + board[4] + " | " + board[5] + " "
+  thirdline = " " + board[6] + " | " + board[7] + " | " + board[8] + " "
   puts firstline
   puts secondline
   puts thirdline
